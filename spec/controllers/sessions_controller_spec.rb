3e196c0496fb40ef7950800c32a865df664ee054
Hsu <hsu@HsuBuntu.(none)>
2011-06-08 13:20:33 +0100

    Done with sign in

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -64,6 +64,16 @@ render_views
     end
     #end valid entry test
 
+		describe "DELETE 'destroy'" do
+
+				it "should sign a user out" do
+				  test_sign_in(Factory(:user))
+				  delete :destroy
+				  controller.should_not be_signed_in
+				  response.should redirect_to(root_path)
+				end
+			end
+
   end
 
 end
